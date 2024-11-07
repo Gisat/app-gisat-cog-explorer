1f90b11f68d5491a24bebb2be1de8f53f31e4ebf
Andrii Khrystodulov <ander481@gmail.com>
2024-11-07 20:36:54 +0100

    add feature -> metadata parser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 'use client'
 
+import React, { useState, useEffect } from 'react';
+import { useSearchParams } from "next/navigation";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import Map from '@/components/layouts/map'
+import { getCogParams } from '@/utils/get-cog-params';
 
 // Styles
 import classes from '@/styles/Home.module.css';
 import Dashboard from "@/components/layouts/dashboard";
 import Editor from "@/components/layouts/editor";
+import { getCogMetadata } from '@/utils/get-cog-metadata';
 
 /**
  * Todo list:
@@ -15,10 +19,37 @@ import Editor from "@/components/layouts/editor";
  */
 
 export default function Home() {
+
+  // Editor with metadata
+  const searchParams = useSearchParams();
+  const [metadata, setMetadata] = useState<string>(
+    "Welcome to COG Explorer! Enter your COG URL into the data source field to start exploring its metadata."
+  );
+
+  useEffect(() => {
+    async function fetchMetadata() {
+      const cogMetadata = await getCogMetadata(searchParams);
+      if (cogMetadata) {
+        setMetadata(JSON.stringify(cogMetadata, null, 2));
+      } else {
+        setMetadata("Failed to load metadata. Please check the COG URL.");
+      }
+    }
+
+    fetchMetadata();
+  }, [searchParams]);
+
+  // Editor with params
+  const [cogParams, setCogParams] = useState<string>();
+  useEffect(() => {
+    const updatedCogParams = JSON.stringify(getCogParams(searchParams), null, 2);
+    setCogParams(updatedCogParams);
+  }, [searchParams]);
+
   return (
     <main className={classes.main}>
       <PanelGroup autoSaveId="app-main-panelgroup" direction="horizontal" className={classes.panelGroup}>
-        <Panel defaultSize={30} minSize={30} className={classes.leftPanel}>
+        <Panel defaultSize={25} minSize={25} className={classes.leftPanel}>
           <Dashboard />
         </Panel>
         <PanelResizeHandle className={classes.handleSide} />
@@ -29,11 +60,18 @@ export default function Home() {
             </Panel>
             <PanelResizeHandle className={classes.handleSide} />
             <Panel className={classes.consolePanel} defaultSize={20} collapsible={true} minSize={20} maxSize={70}>
-              <Editor />
+              <PanelGroup direction="horizontal">
+                <Panel defaultSize={50}>
+                  <Editor content={metadata} />
+                </Panel>
+                <PanelResizeHandle className={classes.handleSide} />
+                <Panel defaultSize={50}>
+                  <Editor content={cogParams} />
+                </Panel>
+              </PanelGroup>
             </Panel>
           </PanelGroup>
         </Panel>
-        <PanelResizeHandle />
       </PanelGroup>
     </main>
   );
--- src/components/layouts/editor/index.tsx
+++ src/components/layouts/editor/index.tsx
@@ -1,27 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { useSearchParams } from "next/navigation";
 import MonacoEditor from '@monaco-editor/react';
-import { getCogParams } from '@/utils/get-cog-params';
 
 interface EditorProps {
-	initialContent: string;
+	content: string | undefined;
 }
 
-const Editor: React.FC<EditorProps> = ({ initialContent }) => {
-	const searchParams = useSearchParams();
-	const cogUrl = searchParams.get("cogUrl");
-
-	const [value, setValue] = useState<string>(initialContent);
-
-	useEffect(() => {
-		const updatedValue = JSON.stringify(getCogParams(searchParams), null, 2);
-		setValue(updatedValue);
-	}, [searchParams]);
-
+const Editor: React.FC<EditorProps> = ({ content }) => {
 	return (
 		<MonacoEditor
 			language="json"
-			value={value}
+			value={content}
 			options={{ theme: 'vs-light' }}
 		/>
 	);
