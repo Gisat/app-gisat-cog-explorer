f229efa533c59a36d88810478ef83d3a7e71ea48
PavelVlach <vlach1989@gmail.com>
2024-12-09 08:08:28 +0100

    line separators

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -2,8 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import React from 'react';
-import { Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 
 import { MainErrorFallback } from '@/components/errors/main';
